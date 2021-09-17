a75d1916d834a653264e576b52f804cdd365bf51
vurdaneta@cobeca.com <vurdaneta@cobeca.com>
2021-09-17 18:52:03 -0400

    Actualización de cambios

--- barchart.py
+++ barchart.py
@@ -4,18 +4,16 @@ import random
 r = lambda: random.randint(0,255)
 root = Tk()
 
-root.minsize(320,240)
+root.minsize(320,400)
 
 canvas_height = 23
-canvas_width = 315
+canvas_width = 1024
 
 w = Canvas(root, width=canvas_width, height=canvas_height)
 w.pack()
-w.create_rectangle(5, canvas_height, canvas_width, 2, fill='#%02X%02X%02X' % (r(),r(),r()))
 w.create_rectangle(5, canvas_height, 100, 2, fill='#%02X%02X%02X' % (r(),r(),r()))
-w.create_text(canvas_width/2, canvas_height / 2, text='Tabla 0', fill='white')
+w.create_rectangle(100, canvas_height, canvas_width, 2, fill='#%02X%02X%02X' % (r(),r(),r()))
+w.create_text(canvas_width/2, canvas_height / 2, text='Tabla 0', fill='black')
 
-a = 1.0 # More complicated code creates this float between 0.00 and 1.00. It is a percentage of the desired 'blue rectangle' width
-b = int(a * canvas_width)
 
 root.mainloop() 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from typing import Dict, List, Tuple
 import math
+from tkinter import Tk, Canvas, Label
+import random
 
 class SimuladorMemoria:
-    CANT_DIRECCIONES: int = 1024
+    CANT_DIRECCIONES: int = 32
     _ultimoAcceso: int = 1
     _paginaActual: int = 0
     _procesos: List[int] = []
@@ -10,13 +12,27 @@ class SimuladorMemoria:
     _memoriaPrincipal: List[Dict[int, int]] = []
     _tabla: List[List[Dict[str, int]]] = []
     _contadorFalloPagina: int = 0
+    _root: Tk
 
     def _getLocalizacionMemoria(self, pid: int, localizacion: int):
         if(localizacion < 0 or localizacion > self._procesos[pid]):
             raise ValueError("Localización de memoria inválida para el proceso")
         return int(int(math.ceil(float(float(localizacion)/float(self._paginaSize)))) - 1)
 
-    def __init__(self):
+    def __init__(self, root):
+        r = lambda: random.randint(0,255)
+        self._root = root
+        self._root.minsize(400,600)
+
+        canvas_tabla_height = 23
+        canvas_memoria_height = 50
+        canvas_width = 1024
+
+        label = Label(root,text='Simulador de Paginación en Memoria')
+        label.pack()
+        w = Canvas(root, width=(canvas_width + 10), height=canvas_memoria_height)
+        w.pack()
+
         numeroPaginas: int = 0
         contadorProgramas: int = 0
         proArchivo = open("plist.txt", "r")
@@ -24,9 +40,9 @@ class SimuladorMemoria:
             item: List[str] = line.strip().split()
             if item:
                 pid: int = int(item[0])
-                memLocation: int = int(item[1])
-                self._procesos.insert(pid, memLocation)
-                numeroPaginas = int(math.ceil(float(memLocation)/float(self._paginaSize)))
+                memSize: int = int(item[1])
+                self._procesos.insert(pid, memSize)
+                numeroPaginas = int(math.ceil(float(memSize)/float(self._paginaSize)))
                 proceso: List[Dict[str, int]] = []
                 for np in range(numeroPaginas):
                     proceso.insert(np, { 'numPag': self._paginaActual, 'enMemoria': 0, 'ultAcceso': 0 })
@@ -36,7 +52,7 @@ class SimuladorMemoria:
         
         proArchivo.close()
         numeroPaginas: int = int(math.ceil(int(self.CANT_DIRECCIONES) / int(self._paginaSize)))
-        paginasPorPrograma: int = int(numeroPaginas) / int(contadorProgramas)
+        paginasPorPrograma: int = int(int(numeroPaginas) / int(contadorProgramas))
 
         for i in range(contadorProgramas):
             for j in range(paginasPorPrograma):
@@ -44,10 +60,29 @@ class SimuladorMemoria:
                 self._tabla[i][j]['enMemoria'] = 1
                 self._tabla[i][j]['ultAcceso'] = self._ultimoAcceso
                 self._ultimoAcceso += 1
-            
+        
+        desde = 10
+        hasta = int((canvas_width - 200) / numeroPaginas)
+        for programa in range(self._tabla.__len__()):
+            for pagina in self._tabla[programa]:
+                w.create_rectangle(desde, canvas_tabla_height, hasta, 2, fill='green' if pagina['enMemoria'] == 1 else 'red')
+                w.create_text(desde + 10, canvas_tabla_height / 2, text=pagina['numPag'], fill='black' if pagina['enMemoria'] == 1 else 'white')
+                desde = hasta + 1;
+                hasta += int((canvas_width - 200) / numeroPaginas)
+        
+        desde = 10
+        hasta = int((canvas_width) / self._memoriaPrincipal.__len__())
+        contador = 0
+        for paginaEnMemoria in self._memoriaPrincipal:
+            w.create_rectangle(desde, canvas_tabla_height + 4, hasta, canvas_memoria_height, fill='green')
+            w.create_text(desde + 30, (canvas_memoria_height + canvas_tabla_height) / 2, text='Pag. {}'.format(paginaEnMemoria['numPag']), fill='black' if paginaEnMemoria['enMemoria'] == 1 else 'white')
+            desde = hasta + 1;
+            hasta += int((canvas_width) / self._memoriaPrincipal.__len__())
+            contador += 1
+
         numeroPaginas = paginasPorPrograma * contadorProgramas
         #tempSize: int = 0
-
+'''
         memArchivo = open("mlist.txt", "r")
         for line in memArchivo.readlines():
             item: List[str] = line.strip().split()
@@ -69,5 +104,7 @@ class SimuladorMemoria:
                     self._memoriaPrincipal[indiceIntercambio]['enMemoria'] = 1
                     self._memoriaPrincipal[indiceIntercambio]['ultAcceso'] = self._ultimoAcceso
 
-
-prueba = SimuladorMemoria()
+'''
+root = Tk()
+prueba = SimuladorMemoria(root)
+root.mainloop()
